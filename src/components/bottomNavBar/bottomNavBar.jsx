9b0c7b90fbe803ab87cb6349b74e67346f307356
Ekep Obasi <arreyettaekep@gmail.com>
2023-03-27 11:21:43 +0100

    created buttom navigation-bar component folder and setup styles

---
+++ src/components/bottomNavBar/bottomNavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StyledButtomNav from "./bottomNavBar.styles";
+
+const bottomNavBar = () => {
+  return (
+    <StyledButtomNav>
+      
+    </StyledButtomNav>
+  )
+  
+};
+
+export default bottomNavBar;
