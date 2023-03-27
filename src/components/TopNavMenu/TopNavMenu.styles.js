87812865eed1b63ad214451612971521f8347c3d
Mark Carrington <frumarkcarrington@gmail.com>
2023-03-27 15:56:55 +0100

    The Drawer menu This commit is for the building for the drawer menu. This menu has no addition components but it is a modification of the top navigation component

--- src/components/TopNavMenu/TopNavMenu.styles.js
+++ src/components/TopNavMenu/TopNavMenu.styles.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const NavContainer = styled.nav`
@@ -64,4 +65,93 @@ const NavContainer = styled.nav`
   }
 `;
 
-export { NavContainer };
+const DrawerMenuNavigation = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 85%;
+  height: 100vh;
+  background: #ffffff;
+  box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 20px 20px 0px;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0.5cm 0.5cm;
+  transform: ${(props) =>
+    props.show ? "translateX(0%)" : "translateX(-100%)"};
+  transition: all 0.1s 250ms ease-out;
+  .text-style {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 20px;
+  }
+  .top-part {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1cm;
+  }
+  .close-icon {
+    width: 1cm;
+    height: 1cm;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    cursor: pointer;
+    svg {
+      width: 48px;
+      height: 48px;
+    }
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+  }
+  .menu-links {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+  .nav-page-link {
+    display: flex;
+    align-items: center;
+    padding: 12px 20px;
+    width: 280px;
+    margin: 0.25cm 0;
+    border-radius: 50px;
+    cursor: pointer;
+    svg {
+      width: 24px;
+      height: 24px;
+      margin-right: 1cm;
+    }
+    span {
+      font-size: 20px;
+    }
+    &:hover {
+      background: #b9debb;
+    }
+  }
+`;
+
+const Backdrop = styled.div`
+  position: ${(props) => (props.show ? "fixed" : "none")};
+  transition: all 0.5s 250ms ease-in;
+
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: ${(props) => (props.show ? "2" : "-1")};
+`;
+
+export { NavContainer, DrawerMenuNavigation, Backdrop };
