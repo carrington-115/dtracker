0be9a367efcb770dcb9db58e85a0db71b48e1aa1
Ekep Obasi <arreyettaekep@gmail.com>
2023-03-28 03:06:39 +0100

    Built the Pop-up Modal Component by passing Props (such as: icon, message, exitButton and continueButton) to child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 import "./App.css";
-import { HiOutlineCamera } from "react-icons/hi";
-import BottomNavBar from "./Components/bottomNavBar/bottomNavBar";
+import { BsCheckCircle } from "react-icons/bs";
+import Modal from "./Components/modal/modal";
 import Button from "./Components/button/button";
 
 function App() {
   return (
     <div className="App">
-      <BottomNavBar />
-      <div>
-        <Button
-          name="outlined"
-          variance="outlined"
-          startIcon={<HiOutlineCamera />}
-        />
-        <Button
-          name="Create Your Account"
-          variance="contained"
-          color="#226E27"
-        />
-        <Button name="text Button" variance="text" />
-        <Button variance="logo" name={<HiOutlineCamera size={23} />} />
-      </div>
+      <Modal
+        icon={<BsCheckCircle color="#226E27" size={96} />}
+        message="Your Tracking data has been recorded"
+        exitButton={
+          <Button name="View Record" variance="contained" color="#226E27" />
+        }
+        continueButton={
+          <Button name="continue To Dashboard" variance="outlined" />
+        }
+      />
     </div>
   );
 }
