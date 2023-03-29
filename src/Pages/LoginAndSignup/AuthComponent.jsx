b8db3b6e559e773a83260ce96a9d6c54a46efbc3
Mark Carrington <frumarkcarrington@gmail.com>
2023-03-29 03:05:52 +0100

    Fixed the bug on the ath header component page

--- src/Pages/LoginAndSignup/AuthComponent.jsx
+++ src/Pages/LoginAndSignup/AuthComponent.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import LoginHeader from "../../components/Loginheader/LoginHeader";
 import { AuthComponentContainer, LoginPageLinks } from "./authcomponent.styles";
-import line from "../../assets/line.svg";
 import { Outlet } from "react-router-dom";
 
 function AuthComponent() {
