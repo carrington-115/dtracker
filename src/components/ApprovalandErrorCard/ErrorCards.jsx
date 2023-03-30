f5f0413e4166917e9344c965a9858f0f9d6dc9d4
Mark Carrington <frumarkcarrington@gmail.com>
2023-03-30 17:16:53 +0100

    Created the error component container The styles for the component were reused ones from the Card component. This component also used props to give the reusability factor to this card in different situations in the app.

--- src/components/ApprovalandErrorCard/ErrorCards.jsx
+++ src/components/ApprovalandErrorCard/ErrorCards.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import styled from "styled-components";
+import { ErrorCardContainer } from "./Cards.styles";
+import { BiArrowBack } from "react-icons/bi";
 
-function ErrorCards() {
-  return <div>ErrorCards</div>;
+function ErrorCards({
+  messageIcon,
+  message,
+  firstButtonAction,
+  secondButtonAction,
+  firstButtonName,
+  secondButtonName,
+}) {
+  return (
+    <MainContainer>
+      <ErrorCardContainer>
+        <div className="top-close-btn">
+          <div className="icon">{<BiArrowBack />}</div>
+        </div>
+        <div className="message-icon-container">
+          <div className="icon">{messageIcon}</div>
+        </div>
+        <div className="message">
+          <p>{message}</p>
+        </div>
+        <div className="btns">
+          <button className="first-action-btn" onClick={firstButtonAction}>
+            {firstButtonName}
+          </button>
+          <button className="second-action-btn" onClick={secondButtonAction}>
+            {secondButtonName}
+          </button>
+        </div>
+      </ErrorCardContainer>
+      <div className="backdrop" />
+    </MainContainer>
+  );
 }
 
 export default ErrorCards;
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  .backdrop {
+    position: fixed;
+    z-index: 10;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`;
